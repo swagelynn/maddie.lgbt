06f4734942114db69b600e477cff57710bbb9804
s4midev <astingraye@proton.me>
2025-11-24 18:09:16 +0000

    update organisation

--- src/index.tsx
+++ src/index.tsx
@@ -66,7 +66,7 @@ function CoolPeople() {
 
 	return (
 		<PageSection
-			className={'col-span-1 row-span-1'}
+			className={'col-start-4 row-start-1'}
 			title="friends"
 			innerClassName="justify-center items-center relative"
 		>
@@ -139,20 +139,12 @@ function App() {
 		soundcloud: 'https://soundcloud.com/swagelynn',
 	};
 
+	const bioStrings = ['maddie', 'she/her', 'programmer'];
+
 	return (
-		<div className="text-accent bg-black grid grid-cols-3 grid-rows-[1fr_1fr_1fr] h-screen w-screen select-none gap-2 p-2">
+		<div className="text-accent bg-black grid grid-cols-4 grid-rows-[2fr_2fr_2fr] h-screen w-screen select-none gap-2 p-2">
 			<PageSection
-				className={'col-span-1 row-span-3'}
-				title="bio"
-				innerClassName="items-center justify-center text-xl font-thin gap-8"
-			>
-				<img src="/pfp.png" className="w-1/2 rounded-md" />
-				<h1>maddie</h1>
-				<h1>she/her</h1>
-				<h1>programmer</h1>
-			</PageSection>
-			<PageSection
-				className={'col-span-1 row-span-2'}
+				className={'row-start-1 row-end-3 col-start-1 col-end-2'}
 				title="music"
 				innerClassName="items-center justify-center col gap-4"
 			>
@@ -173,7 +165,7 @@ function App() {
 				/>
 			</PageSection>
 			<PageSection
-				className={'col-span-1 row-span-1'}
+				className={'row-start-3 row-end-4 col-start-1 col-end-1'}
 				title="links"
 				innerClassName="justify-center items-center gap-4 font-light text-xl"
 			>
@@ -193,13 +185,30 @@ function App() {
 					</>
 				))}
 			</PageSection>
-			<CoolPeople />
 			<PageSection
-				className={'col-span-2 row-span-1'}
-				title="super awesome window"
-				innerClassName="items-center justify-center col gap-4"
+				className={'col-start-2 col-end-4 row-start-1 row-end-4'}
+				title="bio"
+				innerClassName="items-center justify-center text-xl font-thin gap-8"
 			>
-				<h1>[pretend there's something here]</h1>
+				<img src="/pfp.png" className="w-1/3" />
+				<div className="flex flex-row gap-6 items-center justify-center w-full">
+					{bioStrings.map((s, i) => (
+						<>
+							<h1>{s}</h1>
+							{i !== (bioStrings.length - 1) && <h1 className='text-2xl'>∙</h1>}
+						</>
+					))}
+				</div>
+			</PageSection>
+
+			<CoolPeople />
+
+			<PageSection
+				className={'col-start-4 col-end-4 row-start-2 row-end-4'}
+				title="blog"
+				innerClassName="items-center justify-center text-xl font-thin gap-8"
+			>
+				<h1>[blog soon]</h1>
 			</PageSection>
 		</div>
 	);
